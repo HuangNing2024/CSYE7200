6d4a3d066a1e264763ade9ac7ea16fd1e9b8fc5d
Huangning <huang.ni@husky.neu.edu>
2020-04-16 02:12:33 -0400

    add feature

--- DataSearchSystems/app/models/PredictionFormat.scala
+++
@@ -1,6 +0,0 @@
-package models
-
-case class PredictionFormat(pdDistrict:String, street:String, weather: String, weekday: Int,  isNight:Int,
-hour:Int, month:Int, season:Int, occurence:Int, predition:Double)
-
-
